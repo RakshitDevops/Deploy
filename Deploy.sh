4bcf73c76006c94f81ba968a40bbd272b7482504
Sathish1240 <93370862+Sathish1240@users.noreply.github.com>
2021-11-24 23:09:42 -0600

    Create Deploy.sh

---
+++ Deploy.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "download war file"
+wget http://3.17.185.241:8081/nexus/service/local/repositories/Sathish/content/Nexus/sparkjava/20/sparkjava-20.war
+
+echo "downloaded war file successfully"
