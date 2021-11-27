31999a89a268e5be416433f89e88ae5770f7c890
Sathish1240 <93370862+Sathish1240@users.noreply.github.com>
2021-11-26 23:48:18 -0600

    Update Deploy.sh

--- Deploy.sh
+++ Deploy.sh
@@ -2,6 +2,8 @@
 echo "download war file"
 version=$1
 
+url http://3.144.213.192:8081/nexus/service/local/repositories/Sathish/content/Nexus/sparkjava/$version/sparkjava-$version.war
+
 wget http://3.144.213.192:8081/nexus/service/local/repositories/Sathish/content/Nexus/sparkjava/1.0/sparkjava-1.0.war
 
 echo "downloaded war file successfully"
